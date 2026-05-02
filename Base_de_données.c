9f2d47043f198e58b7c6c0ff5543b6b303d44a6f
Simon <simonbulard@gmail.com>
2026-05-02 16:47:03 +0200

    Ajout de nouveaux fichiers et modification du main

---
+++ Base_de_données.c
@@ -0,0 +1,23 @@
+#include "fichier.h"
+int main(){
+    int grille_jeu[7][7];
+    int jrouge;
+    int jbleu;
+    int jjaune;
+    int jviolet;
+    typedef struct{
+
+
+
+    }armes;
+    typedef struct{
+
+
+
+    }monstres;
+    typedef struct{
+
+
+
+    }classes;
+}
