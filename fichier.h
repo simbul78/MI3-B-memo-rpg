bfa1d78af2604c38311554a4743eee3ab2fb658d
Simon <simonbulard@gmail.com>
2026-05-02 15:43:53 +0200

    Ajout de nouveaux fichiers et modification du main

---
+++ fichier.h
@@ -0,0 +1,10 @@
+#ifndef FICHIER_H
+#define FICHIER_H
+// 1. Inclusions des bibliothèques standards
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#endif
